a28e2aff7948be108b3b96619d3e09a8a13540b6
Sebastian Nowak <76070960+Zailen-prog@users.noreply.github.com>
2021-07-24 13:55:10 +0200

    c

--- LA-city/js/map-scripts.js
+++ LA-city/js/map-scripts.js
@@ -502,3 +502,12 @@ function onClickProperties(data) {
 //   }
 // }
 // console.log(result);
+
+function resetHeight() {
+  // reset the body height to that of the inner browser
+  document.body.style.height = window.innerHeight + "px";
+}
+// reset the height whenever the window's resized
+window.addEventListener("resize", resetHeight);
+// called to initially set the height.
+resetHeight();
